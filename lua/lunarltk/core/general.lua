3cd52aeb9f8883d102eae56ca61e083b9d9a99b9
Mechanel <nyutanislavsky@qq.com>
2025-12-26 00:19:03 +0800

    珠联璧合恢复name

--- lua/lunarltk/core/general.lua
+++ lua/lunarltk/core/general.lua
@@ -168,8 +168,7 @@ function General:addCompanions(name)
   if type(name) == "table" then
     table.insertTable(self.companions, name)
   elseif type(name) == "string" then
-    local arg = string.split(name, "__")
-    table.insert(self.companions, arg[#arg])
+    table.insert(self.companions, name)
   end
 end
 
@@ -179,7 +178,7 @@ end
 function General:isCompanionWith(other)
   assert(other:isInstanceOf(General))
   if self == other then return false end
-  return table.contains(self.companions, other.trueName) or table.contains(other.companions, self.trueName)
+  return table.contains(self.companions, other.name) or table.contains(other.companions, self.name)
     or (not not string.find(self.name, "lord") and (other.kingdom == self.kingdom or other.subkingdom == self.kingdom))
     or (not not string.find(other.name, "lord") and (self.kingdom == other.kingdom or self.subkingdom == other.kingdom))
     or (not not string.find(self.name, "all_comp") or not not string.find(other.name, "all_comp")) -- all_comp 所有都珠联璧合
