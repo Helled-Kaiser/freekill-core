c6bd3b32b26d5c727e33e4bb1a6650a81976a2b2
RalphR <1715333874@qq.com>
2026-01-09 15:40:39 +0800

    给onAcquire加了个参数，用于获得他人技能时偷mark

--- lua/lunarltk/core/skill_skeleton.lua
+++ lua/lunarltk/core/skill_skeleton.lua
@@ -40,7 +40,7 @@
 ---@field public max_use_time table<integer, integer?> @ 该技能在各时机内最大的使用次数
 ---@field public max_branches_use_time? table<string, table<integer, integer?>?> | fun(self: SkillSkeleton, player: Player): table<string, table<integer, integer?>?>? @ 该技能的最大使用次数——任意标签（内部有独立的时段细分）
 ---@field public addTest fun(self: SkillSkeleton, fn: fun(room: Room, me: ServerPlayer)) @ 测试函数
----@field public onAcquire fun(self: SkillSkeleton, player: ServerPlayer, is_start: boolean) @ 获得技能时执行的函数
+---@field public onAcquire fun(self: SkillSkeleton, player: ServerPlayer, is_start: boolean, src: ServerPlayer) @ 获得技能时执行的函数
 ---@field public onLose fun(self: SkillSkeleton, player: ServerPlayer, is_death: boolean) @ 失去技能时执行的函数
 ---@field public addEffect fun(self: SkillSkeleton, key: "distance", data: DistanceSpec, attribute: nil): SkillSkeleton
 ---@field public addEffect fun(self: SkillSkeleton, key: "prohibit", data: ProhibitSpec, attribute: nil): SkillSkeleton
@@ -731,7 +731,7 @@ function SkillSkeleton:createViewAsSkill(_skill, idx, key, attr, spec)
   return skill
 end
 
-function SkillSkeleton:onAcquire(player, is_start)
+function SkillSkeleton:onAcquire(player, is_start, src)
   local room = player.room
   if self.attached_skill_name then
     for _, p in ipairs(room.alive_players) do
@@ -741,7 +741,7 @@ function SkillSkeleton:onAcquire(player, is_start)
     end
   end
   if self.on_acquire then
-    self.on_acquire(player, is_start)
+    self.on_acquire(player, is_start, src)
   end
 end
 
@@ -792,12 +792,12 @@ function SkillSkeleton:onLose(player, is_death)
 end
 
 -- 获得此技能时，触发此函数
----@param fn fun(self:SkillSkeleton, player:ServerPlayer, is_start:boolean?)
+---@param fn fun(self:SkillSkeleton, player:ServerPlayer, is_start:boolean?, src:ServerPlayer?)
 function SkillSkeleton:addAcquireEffect(fn)
   ---@param player ServerPlayer
   ---@param is_start boolean?
-  self.on_acquire = function (player, is_start)
-    fn(self, player, is_start)
+  self.on_acquire = function (player, is_start, src)
+    fn(self, player, is_start, src)
   end
 end
 
