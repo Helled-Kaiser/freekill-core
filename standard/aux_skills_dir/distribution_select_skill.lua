bbdbb04e17b6d6720fb59542d62358fa07c2e891
超级妖梦厨 <12796194+youmuKon-supreme@user.noreply.gitee.com>
2026-03-06 00:25:48 +0800

    fix

--- standard/aux_skills_dir/distribution_select_skill.lua
+++ standard/aux_skills_dir/distribution_select_skill.lua
@@ -45,7 +45,7 @@ distributionSelectSkill:addAI(Fk.Ltk.AI.newYijiStrategy {
     local data = ai.data[4] -- extra_data
     local available_cards = ai:getEnabledCards()
 
-    if ai.data[3] --[[ cancelable ]] or data.pattern == "" then return {}, 0 end
+    if ai.data[3] --[[ cancelable ]] or data.pattern == "" then return end
 
     return table.random(available_cards, data.max_num), 0
   end,
@@ -53,7 +53,7 @@ distributionSelectSkill:addAI(Fk.Ltk.AI.newYijiStrategy {
     local data = ai.data[4] -- extra_data
     local available_players = ai:getEnabledTargets()
 
-    if ai.data[3] --[[ cancelable ]] then return {}, 0 end
+    if ai.data[3] --[[ cancelable ]] then return end
 
     return table.random(available_players, 1), 0
   end
