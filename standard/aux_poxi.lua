208a482484c481362a2888baf62323f2f3e64d4a
notify <notify-ctrl@qq.com>
2026-03-19 19:46:17 +0800

    fix: aux_poxi中默认值用错随机函数

--- standard/aux_poxi.lua
+++ standard/aux_poxi.lua
@@ -32,7 +32,7 @@ Fk:addPoxiMethod{
       local cards = pile[2]
       local lim = extra_data.min - #ret
       if #cards > lim then
-        table.insertTable(ret, RoomInstance:random(cards, lim))
+        table.insertTable(ret, RoomInstance:tableRandomPick(cards, lim))
         break
       end
       table.insertTable(ret, cards)
