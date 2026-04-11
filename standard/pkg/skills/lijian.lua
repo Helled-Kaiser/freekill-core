827a66ababdf00af21f9e227726213ceb1aed1e0
Helled-Kaiser <914770800@qq.com>
2026-04-11 15:25:40 +0800

    Update lijian.lua
    “出牌阶段限一次”在多数此阶段内“失而复得”的情形下正确处理（角色先后拥有的同名技能相互独立，故不应共享次数限制）；补充fake_subcards以预防飞刀

--- standard/pkg/skills/lijian.lua
+++ standard/pkg/skills/lijian.lua
@@ -2,21 +2,32 @@ local lijian = fk.CreateSkill {
   name = "lijian",
 }
 
+lijian:addLoseEffect(function (self, player, is_death)
+  local hd = table.contains((player.deputyGeneral == "anjiang") and Fk.generals[player:getMark("__heg_deputy")]:getSkillNameList() or {}, self.name)
+  hd = (hd or table.contains((player.general == "anjiang") and Fk.generals[player:getMark("__heg_general")]:getSkillNameList() or {}, self.name))
+  if (player.phase == Player.Play) and not (is_death or hd) then player:setMark('lijianUsdLst-phase', player:usedEffectTimes(self.name, Player.HistoryPhase))
+  end
+end)
+
 lijian:addEffect("active", {
   anim_type = "offensive",
   prompt = "#lijian-active",
-  max_phase_use_time = 1,
+  max_phase_use_time = function(self, player) --= 1,
+    return (1 + player:getMark('lijianUsdLst-phase'))
+  end,
   card_num = 1,
   target_num = 2,
   card_filter = function(self, player, to_select, selected)
     return #selected == 0 and not player:prohibitDiscard(to_select)
   end,
-  target_filter = function(self, player, to_select, selected)
+  target_filter = function(self, player, to_select, selected, selected_cards) --= function(self, player, to_select, selected)
     if #selected < 2 and to_select ~= player and to_select:isMale() then
       if #selected == 0 then
         return true
       else
-        return to_select:canUseTo(Fk:cloneCard("duel"), selected[1])
+        local Duel = Fk:cloneCard("duel")
+        Duel.fake_subcards = selected_cards
+        return to_select:canUseTo(Duel, selected[1]) --canUseTo(Fk:cloneCard("duel"), selected[1])
       end
     end
   end,
