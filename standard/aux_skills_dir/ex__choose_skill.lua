5061cbc9fa33d72e9eec1688f4994b5fe8305243
Helled-Kaiser <914770800@qq.com>
2026-04-11 10:23:49 +0800

    Update ex__choose_skill.lua
    card_filter、target_filter及feasible增加对系列可选参量的支持

--- standard/aux_skills_dir/ex__choose_skill.lua
+++ standard/aux_skills_dir/ex__choose_skill.lua
@@ -4,42 +4,47 @@ local exChooseSkill = fk.CreateSkill{
 
 exChooseSkill:addEffect('active', {
   card_filter = function(self, player, to_select, selected)
-    if #selected >= self.max_c_num then return false end
 
-    if not table.contains(player:getCardIds("he"), to_select) then
-      local pile = self:getPile(player)
-      if not table.contains(pile, to_select) then return false end
+    local b = ((#selected >= self.max_c_num) or (self.will_throw and player:prohibitDiscard(to_select)))
+    b = (b or (self.pattern and (self.pattern ~= "") and not Exppattern:Parse(self.pattern):match(Fk:getCardById(to_select))))
+    for _, id in ipairs(selected) do
+      local L, c, t = Fk:getCardById(id):getNameLength() == Fk:getCardById(to_select):getNameLength(), Fk:getCardById(id), Fk:getCardById(to_select)
+      local P, A, T, C, S, N = t.type == c.type, t.name == c.name, t.trueName == c.trueName, t.color == c.color, t.suit == c.suit, t:compareNumberWith(c)
+      b = (((self.equalColor == false) and C) or (self.equalColor and not C) or ((self.equalNum == false) and N) or (self.equalNum and not N) or b)
+      b = (((self.equalSuit == false) and S) or (self.equalSuit and not S) or ((self.equalType == false) and P) or (self.equalType and not P) or b)
+      b = (((self.equalName == false) and A) or (self.equalName and not A) or ((self.equalTrueName == false) and T) or (self.equalTrueName and not T) or b)
+      b = (((self.equalNameLength == false) and L) or (self.equalNameLength and not L) or b)
     end
-
-    local checkpoint = true
-    local card = Fk:getCardById(to_select)
-
-    if self.will_throw and player:prohibitDiscard(card) then
-      return false
-    end
-
-
-    if self.pattern and self.pattern ~= "" then
-      checkpoint = checkpoint and (Exppattern:Parse(self.pattern):match(card))
-    end
-    return checkpoint
+    return ((table.contains(player:getCardIds("he"), to_select) or table.contains(self:getPile(player), to_select)) and not b)
   end,
   target_filter = function(self, player, to_select, selected, cards)
-    -- 这有啥好判的
-    -- if #cards < self.min_c_num then return end
-    if #selected < self.max_t_num then
-      return table.contains(self.targets, to_select.id)
-    end
+
+    local pids, s = table.connect(table.map(selected, Util.IdMapper), {to_select.id}), Card:getIdList(card)
+    local d = ((not self.targetIdSets) or table.find(self.targetIdSets, function(tb) return fk.isSubOf(pids, tb) end))
+    local f, t, o = player:distanceTo(to_select, nil, nil), to_select:distanceTo(player, nil, nil), to_select:getAttackRange()
+    d = (d and (((not self.FromMeWithCardNum) or fk.compareNonNegativeNum(f, #s, self.FromMeWithCardNum)) and table.contains(self.targets, to_select.id)))
+    d = (d and ((not self.ToMeWithCardNum) or fk.compareNonNegativeNum(t, #s, self.ToMeWithCardNum)) and (#selected < self.max_t_num))
+    d = (d and ((not self.MaxHpWithCardNum) or fk.compareNum(to_select.maxHp, #s, self.MaxHpWithCardNum)))
+    d = (d and ((not self.HpWithCardNum) or fk.compareNum(to_select.hp, #s, self.HpWithCardNum)))
+    d = (d and ((not self.LostHpWithCardNum) or fk.compareNum(to_select:getLostHp(), #s, self.LostHpWithCardNum)))
+    d = (d and ((not self.AreaCardNumWithCardNum) or fk.compareNum(#to_select:getCardIds('hej'), #s, self.AreaCardNumWithCardNum)))
+    d = (d and ((not self.HandcardNumWithCardNum) or fk.compareNum(to_select:getHandcardNum(), #s, self.HandcardNumWithCardNum)))
+    d = (d and ((not self.AtkRgWithCardNum) or fk.compareNum(o, #s, self.AtkRgWithCardNum)) and not (self.BlockPIdsByCIdstr and table.find(s, function(id)
+        return (self.BlockPIdsByCIdstr[tostring(id)] and table.contains(self.BlockPIdsByCIdstr[tostring(id)], to_select.id)) end)))
+    return (d and (not (self.UnlockPIdsByCIdstr and table.find(Fk:getAllCardIds(), function(id)
+        return (self.UnlockPIdsByCIdstr[tostring(id)] and table.contains(self.UnlockPIdsByCIdstr[tostring(id)], to_select.id)) and not table.contains(s, id)
+      end))) and ((not self.PossessCardNumWithCardNum) or fk.compareNum(#to_select:getCardIds('he'), #s, self.PossessCardNumWithCardNum)))
   end,
   feasible = function (self, player, selected, selected_cards, card)
-    if #selected_cards >= self.min_c_num and #selected_cards <= self.max_c_num and
-      #selected >= self.min_t_num and #selected <= self.max_t_num then
-      if self.equal then
-        return #selected_cards == #selected
-      else
-        return true
-      end
+
+    local r, m = ((not self.TargetNumWithCardNum) or fk.compareNum(m, #selected_cards, self.TargetNumWithCardNum)), #selected
+    r = (r and ((not self.targetIdSets) or table.find(self.targetIdSets, function(tb) return table.isEqual(table.map(selected, Util.IdMapper), tb) end)))
+    for _, p in ipairs(selected) do
+      r = (r and ((not self.FromMeWithTargetNum) or fk.compareNonNegativeNum(player:distanceTo(p, nil, nil), m, self.FromMeWithTargetNum)))
+      r = (r and ((not self.ToMeWithTargetNum) or fk.compareNonNegativeNum(p:distanceTo(player, nil, nil), m, self.ToMeWithTargetNum)))
+      r = (r and ((not self.AtkRgWithTargetNum) or fk.compareNonNegativeNum(p:getAttackRange(), m, self.AtkRgWithTargetNum)))
     end
+    return ((#selected_cards >= self.min_c_num) and (m >= self.min_t_num) and ((m == #selected_cards) or not self.equal) and r)
   end,
   target_tip = function(self, player, to_select, selected, selected_cards, card, selectable, extra_data)
     if self.targetTipName then
@@ -54,7 +59,7 @@ exChooseSkill:addEffect('active', {
   max_card_num = function(self) return self.max_c_num end,
 })
 
-exChooseSkill:addAI(Fk.Ltk.AI.newActiveStrategy {
+exChooseSkill:addAI(Fk.Ltk.AI.newActiveStrategy { --摆了
   think = function(self, ai)
     local data = ai.data[4]
     local orig = Fk.skills[data.skillName] or exChooseSkill
@@ -72,7 +77,7 @@ exChooseSkill:addAI(Fk.Ltk.AI.newActiveStrategy {
   end,
 })
 
-exChooseSkill:addAI(Fk.Ltk.AI.newChooseCardsAndPlayersStrategy {
+exChooseSkill:addAI(Fk.Ltk.AI.newChooseCardsAndPlayersStrategy { --摆了
   choose_cards = function (self, ai)
     local data = ai.data[4] -- extra_data
     local available_cards = ai:getEnabledCards()
